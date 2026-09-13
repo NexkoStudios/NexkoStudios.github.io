7d54f716908a431275d950d9bae5bf7a440ef8f4
Diego Viñoles <nexko.contacto@gmail.com>
2026-09-13 19:02:09 -0300

    Implement card visibility on scroll event
    Add functionality to show cards on scroll

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const cards = document.querySelectorAll(
+    ".skill-card, .project-card, .experience-card"
+);
 
+function mostrarCards() {
+
+    cards.forEach(card => {
+
+        const posicion = card.getBoundingClientRect();
+
+        if (posicion.top < window.innerHeight - 80) {
+            card.classList.add("visible");
+        }
+
+    });
+
+}
+
+window.addEventListener("scroll", mostrarCards);
+
+mostrarCards();
